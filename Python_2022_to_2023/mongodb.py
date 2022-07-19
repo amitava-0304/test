fcccbb9538c554f24388d65508d3d74212908e6d
amitava-0304 <71118670+amitava-0304@users.noreply.github.com>
2022-07-19 11:42:33 +0530

    this is my first code

---
+++ Python_2022_to_2023/mongodb.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://amitava_2112:Suman123@python.e0zfy.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
+print(client.list_database_names())
