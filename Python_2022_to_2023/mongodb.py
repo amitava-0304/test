a94165fb67fa8687435c5f1bc07969ef04a7f0fa
amitava-0304 <71118670+amitava-0304@users.noreply.github.com>
2022-07-19 12:30:21 +0530

    this is my first code

--- Python_2022_to_2023/mongodb.py
+++ Python_2022_to_2023/mongodb.py
@@ -7,6 +7,11 @@ d = {
     "email" : "sudhanshu@ineuron.ai",
     "surname" : "kumar"
 }
+d1 = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
